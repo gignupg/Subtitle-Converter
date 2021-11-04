116cc9cd745fd0626a5de5b6125e1d9ec69db86e
gignu <gignupg@gmail.com>
2021-11-04 23:32:46 +0100

    Added new script to create subtitle files with silence indicators

---
+++ Subtitle-to-Audacity-Label-Converter/create-subtitles-with-silence.js
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import { parse, map, filter, formatTimestamp } from 'subtitle';
+import detectCharacterEncoding from 'detect-character-encoding';
+
+let space = 0;
+const firstArg = process.argv[2];
+
+if (firstArg && !isNaN(firstArg)) {
+    space = Number(firstArg);
+}
+
+const encodingTable = {
+    "ISO-8859-1": "latin1",
+    "UTF-8": "utf8"
+};
+
+const homedir = os.homedir();
+
+const downloadDir = path.join(homedir, '/Downloads');
+
+fs.readdir(downloadDir, function (err, files) {
+    if (err) {
+        console.log("File cannot be properly processed for the following reason:", err);
+
+    } else {
+        const srtFiles = files.filter(el => path.extname(el).toLowerCase() === ".srt");
+
+        if (srtFiles && srtFiles.length === 1) {
+            const fileName = path.join(downloadDir, srtFiles[0]);
+
+            // Encoding
+            const fileBuffer = fs.readFileSync(fileName);
+            const fileEncoding = detectCharacterEncoding(fileBuffer);
+
+            let count = 0;
+            let prevEnd = 0;  // ms
+
+            fs.createReadStream(fileName, encodingTable[fileEncoding])
+                .pipe(parse())
+                .pipe(map((node) => {
+                    if (node.type === 'cue') {
+                        const elem = node.data;       // time in ms
+                        const start = formatTimestamp(elem.start);   // time in srt format
+                        const end = formatTimestamp(elem.end);       // time in srt format
+                        const text = elem.text.replace(/\<\/*.*?\>/g, "");
+                        const silence = Math.floor((elem.start - prevEnd) / 1000);   // in seconds
+
+                        count++;
+
+                        if (silence >= 10) {
+                            let silenceIndicator = `${count}\n${formatTimestamp(prevEnd)} --> ${start}\nSilence (${silence})\n\n`;
+
+                            count++;
+                            prevEnd = elem.end;
+
+                            return silenceIndicator + `${count}\n${start} --> ${end}\n${text}\n\n`;
+
+                        } else {
+                            prevEnd = elem.end;
+                            return `${count}\n${start} --> ${end}\n${text}\n\n`;
+                        }
+                    }
+                }))
+                .pipe(filter(elem => elem))
+                .pipe(fs.createWriteStream(`${downloadDir}/subtitles-with-silence.srt`, encodingTable[fileEncoding]));
+
+        } else {
+            console.log("Conversion failed. Make sure you are in the Downloads folder and there is no more than one srt file present!");
+        }
+    }
+});
